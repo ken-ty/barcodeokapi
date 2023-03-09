0b1432e92da4f078392800c45315eda936dd40a4
todoggler <kentokuraCS@gmail.com>
2023-03-10 00:14:20 +0900

    feat: viewModel, Logic を切り出した形に置き換え
    別のrepositoryからコピーしてきた. これを土台にアーキテクチャから再定義する

---
+++ barcodeokapi/lib/provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_sample/count_data.dart';
+
+/// タイトルstateのプロバイダ
+final titleProvider = Provider<String>((ref) => 'タイトル');
+
+/// メッセージstateのプロバイダ
+final messageProvider = Provider<String>((ref) => 'メッセージ');
+
+/// カウントstateのプロバイダ ( 後で消す )
+final countProvider = StateProvider<int>((ref) => 0);
+
+/// カウントstateのプロバイダ
+final countDataProvider = StateProvider<CountData>(
+  (ref) => CountData(
+    count: 0,
+    countUp: 0,
+    countDown: 0,
+  ),
+);
