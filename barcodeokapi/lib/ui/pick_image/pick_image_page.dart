5a0011c34c33fe981936eac0e050417d3482b818
todoggler <kentokuraCS@gmail.com>
2023-03-30 20:54:47 +0900

    feat: バージョン調整してimage_pickerで画面を作成

--- barcodeokapi/lib/ui/pick_image/pick_image_page.dart
+++ barcodeokapi/lib/ui/pick_image/pick_image_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 /// 画像取り込み画面
 class PickImagePage extends StatefulWidget {
@@ -9,15 +11,34 @@ class PickImagePage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<PickImagePage> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future _getImage() async {
+    // final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    // setState(() {
+    //   if (pickedFile != null) {
+    //     _image = File(pickedFile.path);
+    //   } else {
+    //     print('No image selected.');
+    //   }
+    // });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(title: const Text('アルバムから画像を取り込む')),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () => print("アルバムを開く"),
-        child: Text("アルバムを開く"),
-      )),
+      body: Column(children: [
+        Center(
+          child: _image == null ? Text('No image selected.') : Image.file(_image!),
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImage,
+        child: Icon(Icons.image),
+      ),
     );
   }
 }
