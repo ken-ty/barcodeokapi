7f74bbb9805a02765d7120b866537502329da803
todoggler <kentokuraCS@gmail.com>
2023-05-27 03:40:12 +0900

    feat: Firebaseを導入

--- barcodeokapi/lib/main.dart
+++ barcodeokapi/lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:barcodeokapi/config/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
