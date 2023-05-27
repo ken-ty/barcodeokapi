9f12b509ce75ff5e12850cf895f69501d4dd250a
todoggler <kentokuraCS@gmail.com>
2023-05-27 18:14:24 +0900

    docs: main の処理にコメントを追加

--- barcodeokapi/lib/main.dart
+++ barcodeokapi/lib/main.dart
@@ -5,11 +5,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  // runApp の前に色々やりたいので, runApp で行われるバインドを先に行う.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase のセットアップ
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  // 指定されたウィジェットをインフレートして画面にアタッチします.
+  // ルート ウィジェットの作成なども行います.
   runApp(
     const ProviderScope(
       child: MyApp(),
