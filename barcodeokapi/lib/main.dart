f808a6ff90a27cd1dcb672c184893c82f30c947f
todoggler <kentokuraCS@gmail.com>
2023-05-27 18:14:47 +0900

    feat: crashlytics 有効化

--- barcodeokapi/lib/main.dart
+++ barcodeokapi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:barcodeokapi/config/app.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,6 +13,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // 捕捉されなかった ( = main まで catch されずに到達した ) すべての「致命的」エラーを
+  // フレームワークから Crashlytics に渡します.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   // 指定されたウィジェットをインフレートして画面にアタッチします.
   // ルート ウィジェットの作成なども行います.
